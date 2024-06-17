084833997ea0d365844ebeb4daf6c2f4f8208979
daoudabdelmadjid <daoudzino14@gmail.com>
2024-06-17 17:55:40 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+(function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              
+              { src: 'images/slides/patrick-langwallner-vW52bhIkQ3I-unsplash.jpg' },
+              { src: 'images/slides/thibault-penin-a8r2KKLSntA-unsplash.jpg' },
+              { src: 'images/slides/photo_2024-04-29_23-32-28.jpg' },
+              
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+ 
